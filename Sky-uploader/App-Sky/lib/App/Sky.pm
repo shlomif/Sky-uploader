c37031d31f12bc6860ffafc1e5ce2e8a796d410c
Shlomi Fish <shlomif@shlomifish.org>
2013-05-09 14:27:11 +0300

    Got the tests to pass.

--- Sky-uploader/App-Sky/lib/App/Sky.pm
+++ Sky-uploader/App-Sky/lib/App/Sky.pm
@@ -3,5 +3,14 @@ package App::Sky;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
+=head1 NAME
+
+App::Sky - wrapper to rsync/etc. to upload files to a remote server and give
+download links.
+
+=cut
+
 1;
 
