b7a70e7976d633565072c31185d1a8387b2397bc
Shlomi Fish <shlomif@shlomifish.org>
2013-05-31 20:34:26 +0300

    Fix a bug and add validation for the config.

--- Sky-uploader/App-Sky/lib/App/Sky/CmdLine.pm
+++ Sky-uploader/App-Sky/lib/App/Sky/CmdLine.pm
@@ -67,11 +67,73 @@ EOF
 
     my $filename = shift(@{$self->argv()});
 
-    my $dist_config_dir = File::HomeDir->my_dist_config( 'App-Sky' );
+    my $dist_config_dir = File::HomeDir->my_dist_config( 'App-Sky', {create => 1}, );
 
     my $config_fn = File::Spec->catfile($dist_config_dir, 'app_sky_conf.yml');
 
     my $config = LoadFile($config_fn);
+
+    # Validate the configuration
+    {
+        if (! exists ($config->{default_site}))
+        {
+            die "A 'default_site' key must be present in the configuration.";
+        }
+
+        my $sites = $config->{sites};
+        if (ref($sites) ne 'HASH')
+        {
+            die "sites key must be a hash.";
+        }
+
+        foreach my $k (keys(%$sites))
+        {
+            my $v = $sites->{$k};
+
+            my $base_upload_cmd = $v->{base_upload_cmd};
+            if (ref ($base_upload_cmd) ne 'ARRAY')
+            {
+                die "base_upload_cmd for site '$k' is not an array.";
+            }
+
+            foreach my $kk (qw(dest_upload_prefix dest_upload_url_prefix))
+            {
+                my $s = $v->{$kk};
+                if (not
+                    (
+                        defined($s) && (ref($s) eq '') && ($s =~ m/./)
+                    )
+                )
+                {
+                    die "$kk for site '$k' is not a string.";
+                }
+            }
+
+
+
+            my $sections = $v->{sections};
+            if (ref ($sections) ne 'HASH')
+            {
+                die "Sections for site '$k' is not a hash.";
+            }
+
+            foreach my $sect_k (keys (%$sections))
+            {
+                my $sect_v = $sections->{$sect_k};
+
+                if (! defined($sect_v->{basename_re}) or ref($sect_v->{basename_re} ne ''))
+                {
+                    die "Section '$sect_k' at site '$k' must contain a basename_re";
+                }
+
+                if (! defined($sect_v->{target_dir}) or ref($sect_v->{target_dir} ne ''))
+                {
+                    die "Section '$sect_k' at site '$k' must contain a target_dir";
+                }
+            }
+        }
+
+    }
 
     if (reftype($config) ne 'HASH')
     {
